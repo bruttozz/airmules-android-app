ab383ddb11209f574bbc4ceb4eae53895ef0a20e
incidr <lavashell@gmail.com>
2018-11-30 12:17:12 -0500

    Added Flight functionality to Request Details and fixed compilation bug

--- app/src/main/java/com/example/asthana/airmuleschat/RequestDetailActivity.java
+++ app/src/main/java/com/example/asthana/airmuleschat/RequestDetailActivity.java
@@ -2,6 +2,7 @@ package com.example.asthana.airmuleschat;
 
 import android.content.Intent;
 import android.icu.text.UnicodeSetSpanner;
+import android.os.AsyncTask;
 import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -18,9 +19,18 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import org.w3c.dom.Text;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.lang.ref.ReferenceQueue;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class RequestDetailActivity extends BaseMenuActivity {
 
@@ -53,6 +63,12 @@ public class RequestDetailActivity extends BaseMenuActivity {
     private static final String WIDTH = "width";
     private static final String CUSTOMER = "customer";
     private static final String MULE = "mule";
+    static final String API_URL = "https://aviation-edge.com/v2/public/flights?key=782cbd-deb8af&flightIata=";
+    private double latitude;
+    private double longitude;
+    private double planedir;
+    private String depart;
+    private String arrive;
 
     private DatabaseReference mDatabase;
     private FirebaseAuth mFirebaseAuth;
@@ -84,7 +100,21 @@ public class RequestDetailActivity extends BaseMenuActivity {
 
         btnSignUpOrUnregister = (Button) findViewById(R.id.btnSignUpOrUnregister);
         btnFlight = (Button) findViewById(R.id.btnFlight);
-        // TODO: add function to this button
+        btnFlight.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                parseAirport(loadJSONFromAsset("flights.json"));
+                parseRealTime(loadJSONFromAsset("realtime.json"));
+                Intent myIntent = new Intent(RequestDetailActivity.this, MapsActivity.class);
+                myIntent.putExtra("Latitude",latitude);
+                myIntent.putExtra("Longitude",longitude);
+                myIntent.putExtra("Direction",planedir);
+                myIntent.putExtra("arrTime",arrive);
+                myIntent.putExtra("depTime",depart);
+                RequestDetailActivity.this.startActivity(myIntent);
+                //new getFlightTask().execute();
+            }
+        });
 
         btnPay = (Button) findViewById(R.id.btnPay);
         // TODO: add function to this button
@@ -202,5 +232,120 @@ public class RequestDetailActivity extends BaseMenuActivity {
 
     }
 
+    public String loadJSONFromAsset(String jsonobject) {
+        String json = null;
+        try {
+            InputStream is = getAssets().open(jsonobject);
+            int size = is.available();
+            byte[] buffer = new byte[size];
+            is.read(buffer);
+            is.close();
+            json = new String(buffer, "UTF-8");
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            return null;
+        }
+        return json;
+    }
+
+    protected void parseAirport(String response) {
+        String result= "";
+        if(response == null) {
+            response = "Error with processing the request";
+        }
+        try{
+            JSONObject obj = new JSONObject(response);
+            JSONObject departure = new JSONObject(obj.getString("departure"));
+            JSONObject arrival = new JSONObject(obj.getString("arrival"));
+            result = departure.getString("scheduledTime").substring(11,16) + " to " + arrival.getString("scheduledTime").substring(11,16);
+            depart = departure.getString("scheduledTime");
+            arrive = arrival.getString("scheduledTime");
+        }
+        catch (JSONException e){
+            result = e.getMessage();
+        }
+        //progressBar.setVisibility(View.GONE);
+        Log.i("INFO", response);
+        //responseView.setText(result);
+    }
+
+    protected void parseRealTime(String response) {
+        String result = "";
+        if(response == null) {
+            response = "Error with processing the request";
+        }
+        try{
+            JSONObject obj = new JSONObject(response);
+            JSONObject geography = new JSONObject(obj.getString("geography"));
+            latitude = Double.parseDouble(geography.getString("latitude"));
+            longitude = Double.parseDouble(geography.getString("longitude"));
+            planedir = Double.parseDouble(geography.getString("direction"));
+            result = geography.getString("longitude");
+        }
+        catch (JSONException e){
+            result = e.getMessage();
+        }
+        //progressBar.setVisibility(View.GONE);
+        Log.i("INFO", response);
+        //responseView.setText(result);
+    }
+
+    //Uses the HttpURLConnection and URL libraries to get the result of the request
+    //Uses input/output buffers to read in the results and paste it to the TextView
+    class getFlightTask extends AsyncTask<Void, Void, String> {
+
+        private Exception exception;
+
+        //toggles progress wheel while making the API call
+        protected void onPreExecute() {
+            //progressBar.setVisibility(View.VISIBLE);
+            //responseView.setText("");
+        }
+
+        //runs the API call in the background
+        protected String doInBackground(Void... urls) {
+            //get the flight number from user input
+            //UNCOMMENT HERE WHEN TESTING API need to get this from user data
+            //String flight = flightNum.getText().toString();
+
+            try {
+                //formats the URL containing the API key to add in the flight number (IATA)
+                //UNCOMMENT HERE WHEN TESTING API
+                URL url = new URL(API_URL);//+flight);
+                //open the connection
+                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                try {
+                    //read in the HTML
+                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+                    StringBuilder stringBuilder = new StringBuilder();
+                    String line;
+                    while ((line = bufferedReader.readLine()) != null) {
+                        stringBuilder.append(line).append("\n");
+                    }
+                    bufferedReader.close();
+                    return stringBuilder.toString();
+                }
+                finally{
+                    //close the connection
+                    urlConnection.disconnect();
+                }
+            }
+            catch(Exception e) {
+                Log.e("ERROR", e.getMessage(), e);
+                return null;
+            }
+        }
+
+        //returns with errors if couldn't process the http request or returned with nothing and toggles the progress bar
+        //sends a Log with the response
+        protected void onPostExecute(String response) {
+            if(response == null) {
+                response = "Error with processing the request";
+            }
+            //progressBar.setVisibility(View.GONE);
+            Log.i("INFO", response);
+            //responseView.setText(response);
+        }
+    }
 
 }

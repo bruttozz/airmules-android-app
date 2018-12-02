eed96ab1807e9e1c1e6387c55693b9382dda59ac
aburkato <aburkatovskiy@gmail.com>
2018-12-02 02:24:51 +0000

    Finished the Geo Preferences activity. Not hooked up yet to the request list.

--- app/src/main/java/com/example/asthana/airmuleschat/GeographicalPreferences.java
+++ app/src/main/java/com/example/asthana/airmuleschat/GeographicalPreferences.java
@@ -1,40 +1,51 @@
 package com.example.asthana.airmuleschat;
 
+import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
 public class GeographicalPreferences extends BaseMenuActivity {
-    private static final String DATABASE_TABLE_NAME = "geoPrefs";
+    protected static final String DATABASE_TABLE_NAME = "geoPrefs";
 
     private FloatingActionButton buttonAdd;
 
     private FirebaseAuth mFirebaseAuth;
     private DatabaseReference mDatabase;
     private RecyclerView listPreferences;
+    private FirebaseRecyclerAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_geographical_preferences);
-        
+
         mFirebaseAuth = FirebaseAuth.getInstance();
         mDatabase = FirebaseDatabase.getInstance().getReference();
 
@@ -47,6 +58,12 @@ public class GeographicalPreferences extends BaseMenuActivity {
         });
 
         listPreferences = (RecyclerView)findViewById(R.id.listPreferences);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this.getApplicationContext());
+        linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        listPreferences.setLayoutManager(linearLayoutManager);
+
+        createDatabaseQueryAdapter();
+        listPreferences.setAdapter(adapter);
     }
 
     @Override
@@ -56,6 +73,40 @@ public class GeographicalPreferences extends BaseMenuActivity {
         MenuItem item = menu.findItem(R.id.geo_pref_menu);
         item.setVisible(false);
         return true;
+    }
+
+    private void createDatabaseQueryAdapter(){
+        //based on https://github.com/firebase/FirebaseUI-Android/blob/master/database/README.md
+
+        Query q = mDatabase.child("users").child(mFirebaseAuth.getCurrentUser().getUid()).child(DATABASE_TABLE_NAME).getRef();
+        FirebaseRecyclerOptions<GeoPref> options = new FirebaseRecyclerOptions.Builder<GeoPref>()
+                .setQuery(q, GeoPref.class).build();
+
+        adapter = new FirebaseRecyclerAdapter<GeoPref, GeoPrefHolder>(options) {
+            @Override
+            public GeoPrefHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+                View view = LayoutInflater.from(parent.getContext())
+                        .inflate(R.layout.geo_pref_row, parent, false);
+                return new GeoPrefHolder(view);
+            }
+
+            @Override
+            protected void onBindViewHolder(GeoPrefHolder holder, int position, GeoPref model) {
+                holder.bindTransactionData(model);
+            }
+        };
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        adapter.startListening();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        adapter.stopListening();
     }
 
     private void createAddDialog(){
@@ -90,6 +141,7 @@ public class GeographicalPreferences extends BaseMenuActivity {
             public void onClick(View v) {
                 if(edtTxtCountry.getText().toString().isEmpty()){
                     Toast toast = Toast.makeText(getApplicationContext(),"Country is required", Toast.LENGTH_SHORT);
+                    toast.show();
                     return;
                 }
                 //We will allow the city to be unspecified
@@ -163,5 +215,31 @@ class GeoPref{
 
     public void setCountry(String country) {
         this.country = country;
+    }
+}
+
+class GeoPrefHolder extends RecyclerView.ViewHolder {
+    private GeoPref geoPref;
+    private TextView txtLocation;
+    private Button btnDelete;
+
+    public GeoPrefHolder(View itemView) {
+        super(itemView);
+
+        txtLocation = (TextView) itemView.findViewById(R.id.txtLocation);
+        btnDelete = (Button) itemView.findViewById(R.id.btnDelete);
+    }
+
+    public void bindTransactionData(GeoPref gP) {
+        geoPref = gP;
+
+        txtLocation.setText(GeoPref.createKey(geoPref.getCity(), geoPref.getCountry()));
+        btnDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseDatabase.getInstance().getReference().child("users").child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                        .child(GeographicalPreferences.DATABASE_TABLE_NAME).child(GeoPref.createKey(geoPref.getCity(), geoPref.getCountry())).removeValue();
+            }
+        });
     }
 }

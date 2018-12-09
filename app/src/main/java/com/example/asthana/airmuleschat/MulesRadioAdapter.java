63979216bbfcd4764dd1d76b8611039132467f90
aburkato <aburkatovskiy@gmail.com>
2018-12-09 22:11:13 +0000

    Clear chat after a mule is removed

--- app/src/main/java/com/example/asthana/airmuleschat/MulesRadioAdapter.java
+++ app/src/main/java/com/example/asthana/airmuleschat/MulesRadioAdapter.java
@@ -164,6 +164,7 @@ public class MulesRadioAdapter extends ArrayAdapter<UserClass> {
 
                             if(req.getMule() != null && req.getMule().equals(muleID)) {
                                 FirebaseDatabase.getInstance().getReference().child("requests").child(transactionID).child("mule").removeValue();
+                                clearChat();
                             }
                         }
 
@@ -181,5 +182,12 @@ public class MulesRadioAdapter extends ArrayAdapter<UserClass> {
         }
 
         return v;
+    }
+
+    private void clearChat(){
+        if(transactionID != null) {
+            FirebaseDatabase.getInstance().getReference().
+                    child(PersonalChat.PERSONAL_MESSAGES_CHILD).child(transactionID).removeValue();
+        }
     }
 }
